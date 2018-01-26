af1a81edf5a7367737c3cc79f855ce4b25a5b6db
Robin Radic <robin@radic.nl>
2018-01-26 18:50:40 +0100

    upudat

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,13 +1,22 @@
 <?php
+/**
+ * Part of the Laradic PHP Packages.
+ *
+ * Copyright (c) 2018. Robin Radic.
+ *
+ * The license can be found in the package and online at https://laradic.mit-license.org.
+ *
+ * @copyright Copyright 2018 (c) Robin Radic
+ * @license https://laradic.mit-license.org The MIT License
+ */
+
 namespace Laradic\Tests\IconGenerator;
 
 define('DS', DIRECTORY_SEPARATOR);
 
 use Laradic\Filesystem\Filesystem;
-use Laradic\IconGenerator\Factory;
-use Laradic\Testing\Native\AbstractTestCase;
 
-abstract class TestCase extends AbstractTestCase
+abstract class TestCase extends \PHPUnit_Framework_TestCase
 {
     protected $fonts = [
         'font-aweswome',
